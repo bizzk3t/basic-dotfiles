d21f01137367deb0e580c15a8c4bcaa2618c9876
William Harrison <w@willharrison.org>
2020-11-20 19:38:52 -0600

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-for i in $(ls .[^.]*); do
-  ln -sf "$PWD/$i" "$HOME/$i"
+FILES="$(find . -name '.[^.]*' -not -name .git)"
+for file in FILES; do
+  ln -sf "$PWD/$file" "$HOME/$file"
 done
